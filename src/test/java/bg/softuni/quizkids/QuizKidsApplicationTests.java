53ad0448df7e25b2bd3d075d462af25bbe6126e2
napetkov <n.a.petkov@gmail.com>
2023-11-14 00:12:23 +0200

    Create a project and application properties

---
+++ src/test/java/bg/softuni/quizkids/QuizKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.quizkids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizKidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
