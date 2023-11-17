bcf114b3742193fcebf6aabfb23bdce10a05191b
napetkov <n.a.petkov@gmail.com>
2023-11-17 18:10:41 +0200

    Complete:  - index.html  - login.html  - nav-bar.html  - footer.html

--- src/main/java/bg/softuni/quizkids/controller/TestControllerGetRequests.java
+++ src/main/java/bg/softuni/quizkids/controller/TestControllerGetRequests.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class TestControllerGetRequests {
     @GetMapping("/")
     public String index(){
         return "index";
@@ -17,6 +17,11 @@ public class HomeController {
     @GetMapping("/contact")
     public String contact(){
         return "contact";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 
 }
