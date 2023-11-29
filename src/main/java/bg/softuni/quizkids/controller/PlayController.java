ce73d3b5d1d714f89014e0abdea1bce4a77364b1
napetkov <n.a.petkov@gmail.com>
2023-11-29 13:21:14 +0200

    GetMapping by categoryName in /play/categoryName

--- src/main/java/bg/softuni/quizkids/controller/PlayController.java
+++ src/main/java/bg/softuni/quizkids/controller/PlayController.java
@@ -1,15 +1,18 @@
 package bg.softuni.quizkids.controller;
 
+import bg.softuni.quizkids.models.enums.CategoryName;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/play")
 public class PlayController {
 
-    @GetMapping("/math")
-    public String play(){
+    @GetMapping("/{categoryName}")
+    public String play(@PathVariable("categoryName")CategoryName categoryName){
+
         return "math";
     }
 
