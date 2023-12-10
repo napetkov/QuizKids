f28c235e55e75d528f304426ab656ab1378dbf66
napetkov <n.a.petkov@gmail.com>
2023-12-10 07:30:07 +0200

    Correction of View pages

--- src/test/java/bg/softuni/quizkids/services/impl/PlayServiceImplTest.java
+++ src/test/java/bg/softuni/quizkids/services/impl/PlayServiceImplTest.java
@@ -1,5 +1,6 @@
 package bg.softuni.quizkids.services.impl;
 
+import bg.softuni.quizkids.exceptions.QuestionNotFoundException;
 import bg.softuni.quizkids.models.binding.AddAnswerBindingModel;
 import bg.softuni.quizkids.models.binding.AddQuestionBindingModel;
 import bg.softuni.quizkids.models.dto.QuestionAndAnswerDTO;
@@ -16,56 +17,90 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 class PlayServiceImplTest {
     @Autowired
     private PlayServiceImpl playServiceToTest;
     @Autowired
-    private  QuestionRepository questionRepository;
+    private QuestionRepository questionRepository;
     @Autowired
-    private  UserRepository userRepository;
+    private UserRepository userRepository;
     @Autowired
-    private  UserService userService;
+    private UserService userService;
+    @Autowired
     private QuestionService questionService;
     @Autowired
-    private  ModelMapper modelMapper;
-    private TestData testData = new TestData();
+    private ModelMapper modelMapper;
+    @Autowired
+    private TestData testData;
+
     @BeforeEach
     void setUp() {
         playServiceToTest = new PlayServiceImpl(
-            questionRepository,
-            userRepository,
-            userService,
-            modelMapper
+                questionRepository,
+                userRepository,
+                userService,
+                modelMapper
         );
+        testData.clearAllTestData();
     }
 
     @Test
-    void testFindQuestionByIdToQuestionAndAnswerDTO(){
-        testData.createUserWithRoleUser("author", "topsecret", "pesho", "petrov");
-        UserEntity author = userRepository.findByUsername("author").get();
+    @WithMockUser(username = "admin",roles = {"ADMIN"})
+    void testFindQuestionByIdThrowQuestionNotFound() {
 
-        AddQuestionBindingModel addQuestionBindingModel = new AddQuestionBindingModel();
-        addQuestionBindingModel.setContent("Test Test");
-        addQuestionBindingModel.setCategory("MATH");
-        addQuestionBindingModel.setAnswers(List.of(new AddAnswerBindingModel("test content"),
-                new AddAnswerBindingModel("test content test")));
+        testData.createUserWithRoleUser("admin", "topsecret", "pesho", "petrov");
+
+        UserEntity author = userRepository.findByUsername("admin").get();
+
+        AddQuestionBindingModel addQuestionBindingModel = getAddQuestionBindingModel();
+
+        questionService.addQuestion(addQuestionBindingModel, "admin");
+
+        assertThrows(QuestionNotFoundException.class,
+                () -> playServiceToTest.findQuestionByIdToQuestionAndAnswerDTO(3L));
+    }
+
+    @Test
+    @WithMockUser(username = "admin",roles = {"ADMIN"})
+    void testFindQuestionByIdToQuestionAndAnswerDTO() {
+
+        testData.createUserWithRoleUser("admin", "topsecret", "pesho", "petrov");
+
+        UserEntity author = userRepository.findByUsername("admin").get();
+
+        AddQuestionBindingModel addQuestionBindingModel = getAddQuestionBindingModel();
 
 
-        questionService.addQuestion(addQuestionBindingModel, "author");
+        questionService.addQuestion(addQuestionBindingModel, "admin");
 
         QuestionAndAnswerDTO questionAndAnswerDTO = playServiceToTest
                 .findQuestionByIdToQuestionAndAnswerDTO(1L);
 
-        assertEquals(addQuestionBindingModel.getContent(),questionAndAnswerDTO.getContent());
-        assertEquals(addQuestionBindingModel.getAnswers().size(),questionAndAnswerDTO.getAnswers().size());
-        assertEquals(addQuestionBindingModel.getCategory(),questionAndAnswerDTO.getCategoryName());
+        assertEquals(addQuestionBindingModel.getContent(), questionAndAnswerDTO.getContent());
+        assertEquals(addQuestionBindingModel.getAnswers().size(), questionAndAnswerDTO.getAnswers().size());
+        assertEquals(addQuestionBindingModel.getCategory(), questionAndAnswerDTO.getCategoryName());
 
+    }
+
+    private static AddQuestionBindingModel getAddQuestionBindingModel() {
+        AddQuestionBindingModel addQuestionBindingModel = new AddQuestionBindingModel();
+        addQuestionBindingModel.setContent("Test Test");
+        addQuestionBindingModel.setCategory("MATH");
+        addQuestionBindingModel.setAnswers(List.of(
+                new AddAnswerBindingModel("test content 1"),
+                new AddAnswerBindingModel("test content 2"),
+                new AddAnswerBindingModel("test content 3"),
+                new AddAnswerBindingModel("test content 4")
+                ));
+        return addQuestionBindingModel;
     }
 }
