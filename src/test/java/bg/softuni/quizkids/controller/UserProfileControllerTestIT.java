d78fc5b7dfb7aad6e3b67c92cc64302fe016fd9a
napetkov <n.a.petkov@gmail.com>
2023-12-20 06:08:25 +0200

    Integration test of UserProfileController

---
+++ src/test/java/bg/softuni/quizkids/controller/UserProfileControllerTestIT.java
@@ -0,0 +1,124 @@
+package bg.softuni.quizkids.controller;
+
+import bg.softuni.quizkids.testUtils.TestData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserProfileControllerTestIT {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private TestData testData;
+
+    @BeforeEach
+    void setUp() {
+        testData.clearAllTestData();
+    }
+
+    @AfterEach
+    void tearDown() {
+        testData.clearAllTestData();
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void testGetUserProfile() throws Exception {
+
+        testData.createUserWithRoleAdmin(
+                "admin",
+                "topsecret",
+                "Pesho",
+                "Petrov"
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/profile"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("profile"));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void testEditProfile() throws Exception {
+
+        testData.createUserWithRoleAdmin(
+                "admin",
+                "topsecret",
+                "Pesho",
+                "Petrov"
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/profile/edit"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("edit-profile"));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void testEditUserProfile() throws Exception {
+
+        testData.createUserWithRoleAdmin(
+                "admin",
+                "topsecret",
+                "Pesho",
+                "Petrov"
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/profile/edit")
+                        .param("firstName", "Pesho")
+                        .param("lastName", "Petrov")
+                        .param("email", "pesho@example.com")
+                        .param("age", "13")
+                        .param("city", "Ruse"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/profile"));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void testChangePasswordGet() throws Exception {
+
+        testData.createUserWithRoleAdmin(
+                "admin",
+                "topsecret",
+                "Pesho",
+                "Petrov"
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/profile/edit/changePassword"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("change-password"));
+    }
+
+    //PasswordEncoder do not match passwords properly
+//    @Test
+//    @WithMockUser(username = "admin",roles = {"ADMIN"})
+//    public void testChangePasswordPost() throws Exception {
+//
+//        testData.createUserWithRoleAdmin(
+//                "admin",
+//                "topsecret",
+//                "Pesho",
+//                "Petrov"
+//        );
+//
+//        mockMvc.perform(MockMvcRequestBuilders.post("/users/profile/edit/changePassword")
+//                        .param("oldPassword","topsecret")
+//                        .param("password","newPassword")
+//                        .param("confirmPassword","newPassword")
+//                )
+//                .andExpect(status().is3xxRedirection())
+//                .andExpect(redirectedUrl("/users/logout"));
+//    }
+}
